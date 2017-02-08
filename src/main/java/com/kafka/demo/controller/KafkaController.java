e4c77d3444dcb58cd1efd4c964054eb2d9b72ab8
yuanguanghui <yuanguanghui999@gmail.com>
2017-02-08 16:46:57 +0800

    集成spring

--- src/main/java/com/kafka/demo/controller/KafkaController.java
+++ src/main/java/com/kafka/demo/controller/KafkaController.java
@@ -4,33 +4,27 @@ import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.kafka.demo.service.KafkaService;
-
-import java.util.concurrent.TimeUnit;
 
 @Controller
 @RequestMapping("/kafka")
 public class KafkaController {
 
 	private static final Logger logger = LoggerFactory.getLogger(KafkaController.class);
-	@Resource
-	private KafkaService kafkaService;
-	
-	@RequestMapping("/test")
-	public String test(){
-		logger.info("-------KafkaController--------start-----");
-		System.err.println("---------KafkaController--------start---------");
+
+
+	@Autowired
+	private KafkaTemplate<Integer, String> kafkaTemplate;
+
+	@RequestMapping("/test2")
+	public String test2(){
 		int i = 0;
 		while (i < 10) {
-			kafkaService.sendInfo("testTopic","kafka sendMessage test!" + "  ----- " + i);
-			try {
-				Thread.currentThread().sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			kafkaTemplate.sendDefault(i, "haha" + i);
 			i++;
 		}
 
---
+++ src/main/java/com/kafka/demo/kafka/KafkaConsumer.java
@@ -0,0 +1,16 @@
+package com.kafka.demo.kafka;
+
+/**
+ * Created by yuanguanghui on 2017/2/8.
+ */
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.listener.MessageListener;
+
+public class KafkaConsumer implements MessageListener<Integer, String>{
+
+    //@Override
+    public void onMessage(ConsumerRecord<Integer, String> record) {
+        System.out.println(record);
+    }
+
+}
